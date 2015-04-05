f7ce4c9da623cb7cbce7e7c5f65d8106fc19af1e
Thomas <tagrant@uncg.edu>
2015-04-05 13:39:39 -0400

    Thomas comment

--- app/src/main/java/com/example/vinnie/pixelpicker/MainActivity.java
+++ app/src/main/java/com/example/vinnie/pixelpicker/MainActivity.java
@@ -7,10 +7,12 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    //Thomas is great
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String thomas = "Great";
         setContentView(R.layout.activity_main);
     }
 
